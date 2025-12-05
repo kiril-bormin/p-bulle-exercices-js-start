f42065cfc8030bd1630d729513e56d85eee47336
kiril-bormin <borminnkiril@gmail.com>
2025-12-05 14:34:24 +0100

    feat (exercice) : space-age

--- exercices/gigasecond/gigasecond.js
+++ exercices/gigasecond/gigasecond.js
@@ -3,6 +3,10 @@
 // convenience to get you started writing code faster.
 //
 
-export const gigasecond = () => {
-  throw new Error('Remove this line and implement the function');
+export const gigasecond = (date) => {
+  const gigasecondnumber = 1000000000;
+  let temp = date.getMilliseconds();
+  temp + gigasecondnumber * 1000;
+  let result = temp.getTime();
+  return new Date(result).toString();
 };
--- exercices/space-age/space-age.js
+++ exercices/space-age/space-age.js
@@ -3,6 +3,24 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = () => {
-  throw new Error('Remove this line and implement the function');
+export const age = (planet, ageInSeconds ) => {
+  const planetList = {mercury : 0.2408467, 
+    venus : 0.61519726, 
+    earth : 1.0, 
+    mars : 1.8808158, 
+    jupiter : 11.862615, 
+    saturn : 29.447498, 
+    uranus : 84.016846, 
+    neptune : 164.79132}
+  let result;
+  let resultFloat;
+
+  if(planet in planetList){
+    result = ((ageInSeconds / 365.25 / 86400) / planetList[planet]).toFixed(2);
+    resultFloat = parseFloat(result)
+  } 
+  else{
+    throw new Error("not a planet");
+  } 
+  return resultFloat;
 };
