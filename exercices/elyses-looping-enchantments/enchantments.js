f8c8bd8abb56c073ce379582229699def89a99c6
kiril-bormin <borminnkiril@gmail.com>
2025-12-12 14:34:02 +0100

    feat (exercice) : elyses-looping

--- exercices/elyses-looping-enchantments/enchantments.js
+++ exercices/elyses-looping-enchantments/enchantments.js
@@ -9,8 +9,13 @@
  * @returns {number} number of cards of a single type there are in the deck
  */
 export function cardTypeCheck(stack, card) {
-  // 🚨 Use .forEach
-  throw new Error('Implement the cardTypeCheck function');
+  let i = 0
+  stack.forEach((element) => {
+    if(element == card){
+      i++
+    }
+  });
+  return i;
 }
 
 /**
@@ -21,6 +26,18 @@ export function cardTypeCheck(stack, card) {
  * @returns {number} number of cards that are either odd or even (depending on `type`)
  */
 export function determineOddEvenCards(stack, type) {
-  // 🚨 Use a `for...of` loop
-  throw new Error('Implement the determineOddEvenCards function');
+    let i = 0
+  stack.forEach((element) => {
+    if(element % 2 == 1){
+      if(!type){
+        i++
+      }
+    }
+    else{
+      if(type){
+        i++
+      }
+    }
+  });
+  return i;
 }
